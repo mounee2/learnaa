26cd8d7c6022a370d33d16188a92e0b2eca980a9
Mouneesh <mouneesh0297@gmail.com>
2021-06-21 22:12:21 +0530

    Second Commit

--- src/main/java/com/learnaa/controller/HomeController.java
+++ src/main/java/com/learnaa/controller/HomeController.java
@@ -13,6 +13,7 @@ public class HomeController {
 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
+		System.out.println("Test");
 		return new ModelAndView("home");
 	}
 }
